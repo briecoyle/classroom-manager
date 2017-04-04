138f84d8ee6ec38238f378ffc75854940f4b01ff
Brie Coyle <brie.coyle@gmail.com>
2017-04-04 16:35:25 -0400

    Validate unniqueness of email with test passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid user factory with a first_name, last_name, username, email, and password" do
-    expect(build(:user)).to be_valid
+  describe "test" do
+    it "has a valid user factory with a first_name, last_name, username, email, and password" do
+      expect(build(:user)).to be_valid
+    end
   end
 
-  it 'is invalid without a username' do
-    expect(build(:user, username: nil)).not_to be_valid
+  describe "validations" do
+
+    it 'is invalid without a username' do
+      expect(build(:user, username: nil)).not_to be_valid
+    end
+
+    it 'requires a unique username' do
+      create(:user, username: 'gitflow')
+      user = build(:user, username: 'gitflow')
+      user.valid?
+      expect(user.errors.full_messages).to include("Username has already been taken")
+    end
+
+    it 'is invalid without an email' do
+      expect(build(:user, email: nil)).not_to be_valid
+    end
+
+    it 'in invalid without a valid email (ex. user@gmail.com)' do
+      expect(build(:user, email: "Am I an email?")).not_to be_valid
+    end
+
+    it 'requires a unique email' do
+      create(:user, email: 'test@email.com')
+      user = build(:user, email: 'test@email.com')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'is invalid without a password' do
+      expect(build(:user, email: nil)).not_to be_valid
+    end
+
+    it 'is only valid when the password is a minimum of 8 characters' do
+      expect(build(:user, password: "1234567")).not_to be_valid
+      expect(build(:user, password: "12345678")).to be_valid
+    end
+
+    it 'encrypts the password upon saving' do
+      user = build(:user)
+      user.save
+      expect(user.password_digest).to be_truthy
+      expect(user.password_digest).not_to eq(user.password)
+    end
   end
 
-  it 'requires a unique username' do
-    create(:user, username: 'gitflow')
-    user = build(:user, username: 'gitflow')
-    user.valid?
-    expect(user.errors.full_messages).to include("Username has already been taken")
+  describe 'relationships' do
+
+
   end
 
-  it 'is invalid without an email' do
-    expect(build(:user, email: nil)).not_to be_valid
+  describe 'class methods' do
+
+
   end
 
-  it 'in invalid without a valid email (ex. user@gmail.com)' do
-    expect(build(:user, email: "Am I an email?")).not_to be_valid
-  end
-
-  it 'is invalid without a password' do
-    expect(build(:user, email: nil)).not_to be_valid
-  end
-
-  it 'is only valid when the password is a minimum of 8 characters' do
-    expect(build(:user, password: "1234567")).not_to be_valid
-    expect(build(:user, password: "12345678")).to be_valid
-  end
-
-  it 'encrypts the password upon saving' do
-    user = build(:user)
-    user.save
-    expect(user.password_digest).to be_truthy
-    expect(user.password_digest).not_to eq(user.password)
+  describe 'instance methods' do
+    
   end
 
 end

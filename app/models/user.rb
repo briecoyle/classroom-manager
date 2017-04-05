9424ea15a317ad7d0673329ee26d23052ce06430
Brie Coyle <brie.coyle@gmail.com>
2017-04-04 21:15:03 -0400

    Remove duplicate code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates :password, length: { minimum: 8 }
+
 end

79bf2bb5294dd54017393c596383210dfdf8c493
Brie Coyle <brie.coyle@gmail.com>
2017-04-12 14:13:35 -0400

    Add controller actions for User.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
-  def index
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render new_user_path, notice: "All fields required."
+    end
+  end
+
+  def show
+    if session[:user_id]
+      @user = User.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password)
   end
 end
